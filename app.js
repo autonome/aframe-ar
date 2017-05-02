ef0d867745b2074c10cb26815a75ebf69aa737e2
dietrich ayala <autonome@gmail.com>
2017-05-02 16:39:27 +0800

    move object to script

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ function onReady() {
       facingMode: "environment"
     },
     callback: function() {
-      // do stuff
+      //document.querySelector('#ducky-obj').src = 'ducky.obj'
+      var htmlStr = '<a-obj-model id="duck" src="#ducky-obj" position="0 -800 -4000" rotation="-210 -120 180" scale="8 8 8" material="color: orange"><a-animation easing="linear" attribute="rotation" dur="10000" to="0 0 360" repeat="indefinite"></a-animation> </a-obj-model>';
+      document.querySelector('#aScene').appendChild(new DOMParser().parseFromString(htmlStr, 'text/html').body.firstChild);
     }
   });
 
